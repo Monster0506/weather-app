c317b94acb2e5c58117899a859e1d407ac6f1912
Monster0506 <12682102-monster0506@users.noreply.gitlab.com>
2024-10-23 00:01:57 -0400

    Add advanced functionality and good looks

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template, request
 import requests
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
-# Initialize app
+# Initialize the Flask app
 app = Flask(__name__)
 
 # Load environment variables
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
+
+
+def format_time(timestamp, timezone_offset):
+    """Convert UTC timestamp to local time using the timezone offset"""
+    return datetime.utcfromtimestamp(timestamp + timezone_offset).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
 
 
 @app.route("/")
@@ -27,12 +35,31 @@ def get_weather():
 
     if response.status_code == 200:
         weather_data = response.json()
+
         weather = {
             "city": weather_data["name"],
+            "country": weather_data["sys"]["country"],
+            "coordinates": f"{weather_data['coord']['lat']}, {weather_data['coord']['lon']}",
             "temperature": weather_data["main"]["temp"],
-            "description": weather_data["weather"][0]["description"],
+            "feels_like": weather_data["main"]["feels_like"],
+            "min_temp": weather_data["main"]["temp_min"],
+            "max_temp": weather_data["main"]["temp_max"],
+            "pressure": weather_data["main"]["pressure"],
             "humidity": weather_data["main"]["humidity"],
+            "visibility": weather_data["visibility"],
             "wind_speed": weather_data["wind"]["speed"],
+            "wind_deg": weather_data["wind"]["deg"],
+            "weather_description": weather_data["weather"][0]["description"],
+            "weather_icon": f"http://openweathermap.org/img/wn/{weather_data['weather'][0]['icon']}@2x.png",
+            "clouds": weather_data["clouds"]["all"],
+            "sunrise": format_time(
+                weather_data["sys"]["sunrise"], weather_data["timezone"]
+            ),
+            "sunset": format_time(
+                weather_data["sys"]["sunset"], weather_data["timezone"]
+            ),
+            "sunrise_time": weather_data["sys"]["sunrise"],
+            "sunset_time": weather_data["sys"]["sunset"],
         }
         return render_template("weather.html", weather=weather)
     else:
